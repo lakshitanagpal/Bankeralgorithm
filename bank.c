d0d123e0ec56bd9eba4e08b071309fa0911bf7a2
lakshitanagpal <lakshurocks.nagpal@gmail.com>
2015-11-18 22:22:56 +0530

    Update bank.c

--- bank.c
+++ bank.c
@@ -1,151 +1,65 @@
-/*PROGRAM TO IMPLEMENT BANKER'S ALGORITHM
-  *   --------------------------------------------*/
-#include <stdio.h>
-int curr[5][5], maxclaim[5][5], avl[5];
-int alloc[5] = {0,0,0,0,0};
-int maxres[5], running[5], safe=0;
-int count = 0, i, j, exec, r, p,k=1;
- 
-int main()
-{
-    printf("\nEnter the number of resources: ");
-    scanf("%d",&r);
- 
-    for(i=0;i<r;i++)
-    {
-        running[i]=1;
-        count++;
-    }
- 
-    printf("\nEnter the number of processes: ");
-    scanf("%d",&p);
- 
-    for(i=0;i<p;i++)
-    { 
-        printf("\nEnter the resource for instance %d: ",k++);
-        scanf("%d",&maxres[i]);
-    }
- 
-    printf("\nEnter maximum resource table:\n");
-    for(i=0;i<p;i++)
-    {
-        for(j=0;j<r;j++)
-        {
-            scanf("%d",&maxclaim[i][j]);
-        }
-    }
- 
-    printf("\nEnter allocated resource table:\n");
-    for(i=0;i<p;i++)
-    {
-        for(j=0;j<r;j++)
-        {
-            scanf("%d",&curr[i][j]);
-        }
-    }
- 
-    printf("\nThe resource of instances: ");
-    for(i=0;i<r;i++)
-    {
-        printf("\t%d",maxres[i]);
-    }
- 
-    printf("\nThe allocated resource table:\n");
-    for(i=0;i<p;i++)
-    {
-        for(j=0;j<r;j++)
-        {
-            printf("\t%d",curr[i][j]);
-        }
- 
-        printf("\n");
-    }
- 
-    printf("\nThe maximum resource table:\n");
-    for(i=0;i<p;i++)
-    {
-        for(j=0;j<r;j++)
-        {
-            printf("\t%d",maxclaim[i][j]);
-        }
- 
-        printf("\n");
-    }
- 
-    for(i=0;i<p;i++)
-    {
-        for(j=0;j<r;j++)
-        {
-            alloc[j]+=curr[i][j];
-        }
-    }
- 
-    printf("\nAllocated resources:");
-    for(i=0;i<r;i++)
-    {
-        printf("\t%d",alloc[i]);
-    }
- 
-    for(i=0;i<r;i++)
-    {
-        avl[i]=maxres[i]-alloc[i];
-    }
- 
-    printf("\nAvailable resources:");
-    for(i=0;i<r;i++)
-    {
-        printf("\t%d",avl[i]);
-    }
-    printf("\n");
- 
-    //Main procedure goes below to check for unsafe state.
-    while(count!=0)
-    {
-        safe=0;
-        for(i=0;i<p;i++)
-        {
-            if(running[i])
-            {
-                exec=1;
-                for(j=0;j<r;j++)
-                {
-                    if(maxclaim[i][j] - curr[i][j] > avl[j]){
-                        exec=0;
-                        break;
-                    }
-                }
-                if(exec)
-                {
-                    printf("\nProcess%d is executing\n",i+1);
-                    running[i]=0;
-                    count--;
-                    safe=1;
- 
-                    for(j=0;j<r;j++) {
-                        avl[j]+=curr[i][j];
-                    }
- 
-                    break;
-                }
-            }
-        }
-        if(!safe)
-        {
-            printf("\nThe processes are in unsafe state.\n");
-            break;
-        }
-        else
-        {
-            printf("\nThe process is in safe state");
-            printf("\nSafe sequence is:");
- 
-            for(i=0;i<r;i++)
-            {
-                printf("\t%d",avl[i]);
-            }
- 
-            printf("\n");
-        }
-    }
+#include<stdio.h>
+#include<conio.h>
+void main() {
+	int k=0,output[10],d=0,t=0,ins[5],i,avail[5],allocated[10][5],need[10][5],MAX[10][5],pno,P[10],j,rz, count=0;
+
+	printf("\n Enter the number of resources : ");
+	scanf("%d", &rz);
+	printf("\n enter the max instances of each resources\n");
+	for (i=0;i<rz;i++) {
+		avail[i]=0;
+		printf("%c= ",(i+97));
+		scanf("%d",&ins[i]);
+	}
+	printf("\n Enter the number of processes : ");
+	scanf("%d", &pno);
+	printf("\n Enter the allocation matrix \n     ");
+	for (i=0;i<rz;i++)
+	printf(" %c",(i+97));
+	printf("\n");
+	for (i=0;i <pno;i++) {
+		P[i]=i;
+		printf("P[%d]  ",P[i]);
+		for (j=0;j<rz;j++) {
+			scanf("%d",&allocated[i][j]);
+			avail[j]+=allocated[i][j];
+		}
+	}
+	printf("\nEnter the MAX matrix \n     ");
+	for (i=0;i<rz;i++) {
+		printf(" %c",(i+97));
+		avail[i]=ins[i]-avail[i];
+	}
+	printf("\n");
+	for (i=0;i <pno;i++) {
+		printf("P[%d]  ",i);
+		for (j=0;j<rz;j++)
+		 scanf("%d", &MAX[i][j]);
+	}
+	printf("\n");
+	A: d=-1;
+	for (i=0;i <pno;i++) {
+		count=0;
+		t=P[i];
+		for (j=0;j<rz;j++) {
+			need[t][j] = MAX[t][j]-allocated[t][j];
+			if(need[t][j]<=avail[j])
+			 count++;
+		}
+		if(count==rz) {
+			output[k++]=P[i];
+			for (j=0;j<rz;j++)
+			avail[j]+=allocated[t][j];
+		} else
+		 P[++d]=P[i];
+	}
+	if(d!=-1) {
+		pno=d+1;
+		goto A;
+	}
+	printf("\t <");
+	for (i=0;i<k;i++)
+	printf(" P[%d] ",output[i]);
+	printf("> is the saf");
+	getch();
 }
- 
